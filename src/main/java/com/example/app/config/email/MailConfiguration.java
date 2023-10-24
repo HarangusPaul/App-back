c08499478d33c4129170886e6499b0607f685d0c
PaulHarangus <paulharangus@gmail.com>
2023-10-24 05:48:13 -0700

    email

---
+++ src/main/java/com/example/app/config/email/MailConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.app.config.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+
+        //todo:email auth
+        mailSender.setUsername("my@gmail.com");
+        mailSender.setPassword("mypassword");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
--- src/main/java/com/example/app/service/Email/EmailService.java
+++ src/main/java/com/example/app/service/Email/EmailService.java
@@ -1,4 +1,4 @@
 package com.example.app.service.Email;
 
-public class EmailSender {
+public interface EmailService {
 }
